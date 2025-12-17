ce088498b15cee8ca5dabcc9466e010b4e66c259
VoAnhThien <vothien817@gmail.com>
2025-12-17 20:18:58 +0700

    remove nodemodules

--- backend/server.js
+++ backend/server.js
@@ -5,15 +5,14 @@ require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-
+const MONGO_URI = process.env.MONGODB_URI || "mongodb+srv://vothien817_db_user:eMfrO0V4CHTGdjtS@todo.8pwkods.mongodb.net/?appName=todo";
 app.use(cors());
 app.use(express.json());
 
 // MongoDB Connection
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/todoapp', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+mongoose.connect(MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.error(err));
 
 // Todo Schema
 const todoSchema = new mongoose.Schema({
